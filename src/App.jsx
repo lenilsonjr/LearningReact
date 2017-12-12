064a03d6439b11d5f176a187159f8d1c5908ebdd
Lenilson Júnior <me@lenilson.codes>
2017-12-12 20:58:34 -0300

    :construction: Method for state change of deadline

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import './App.css';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017'
+    }
+  }
+
+  changeDeadline() {
+    this.setState({ deadline: 'November 25, 2017' })
+  }
+
   render() {
     return (
       <div className="app">
-        <div className="app-title">Countdown to December 25, 2017</div>
+        <div className="app-title">
+          Countdown to {this.state.deadline}
+        </div>
         <div>
           <div className="clock-days">14 days</div>
           <div className="clock-hours">30 hours</div>
@@ -15,7 +28,9 @@ class App extends Component {
         </div>
         <div>
           <input placeholder='new date' />
-          <button>Submit</button>
+          <button onClick={() => this.changeDeadline()}>
+          Submit
+          </button>
         </div>
       </div>
     )
