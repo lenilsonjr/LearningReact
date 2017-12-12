eca941bf2b11da93780c1d24cbabf1401e4e84f0
Lenilson Júnior <me@lenilson.codes>
2017-12-12 20:48:09 -0300

    :art: Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
 
   render() {
     return (
-      <div>Countdown Champ, App Component</div>
+      <div className="app">
+        <div className="app-title">Countdown to December 25, 2017</div>
+        <div>
+          <div className="clock-days">14 days</div>
+          <div className="clock-hours">30 hours</div>
+          <div className="clock-minutes">15 minutes</div>
+          <div className="clock-seconds">20 seconds</div>
+        </div>
+        <div>
+          <input placeholder='new date' />
+          <button>Submit</button>
+        </div>
+      </div>
     )
   }
 
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App></App>, document.getElementById('root')
+  <App />,
+  document.getElementById('root')
 );
