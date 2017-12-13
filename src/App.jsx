5535637b98a45236ba3fd9a91d9454d62b249315
Lenilson Júnior <me@lenilson.codes>
2017-12-12 22:28:07 -0300

    :art::sparkles: Bootstrap and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Clock from './Clock';
 import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
 
@@ -25,15 +26,16 @@ class App extends Component {
         <Clock 
           deadline={this.state.deadline}
         />
-        <div>
-          <input 
+        <Form inline>
+          <FormControl
+            className="deadline-input"
             placeholder='new date'
             onChange={event => this.setState({newDeadline: event.target.value})}
           />
-          <button onClick={() => this.changeDeadline()}>
+          <Button onClick={() => this.changeDeadline()}>
           Submit
-          </button>
-        </div>
+          </Button>
+        </Form>
       </div>
     )
   }
