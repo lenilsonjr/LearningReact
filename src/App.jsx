ff90b6dea0eefa6cd5d086e3563d23e2b5315162
Lenilson Júnior <me@lenilson.codes>
2017-12-12 21:04:32 -0300

    :sparkles: Change state on user input submit

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deadline: 'December 25, 2017'
+      deadline: 'December 25, 2017',
+      newDeadline: ''
     }
   }
 
   changeDeadline() {
-    this.setState({ deadline: 'November 25, 2017' })
+    this.setState({deadline: this.state.newDeadline});
   }
 
   render() {
@@ -27,7 +28,10 @@ class App extends Component {
           <div className="clock-seconds">20 seconds</div>
         </div>
         <div>
-          <input placeholder='new date' />
+          <input 
+            placeholder='new date'
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
           <button onClick={() => this.changeDeadline()}>
           Submit
           </button>
