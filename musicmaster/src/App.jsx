dedc57463ded731f7ce79c59a3445c76ec8bd943
Lenilson Júnior <me@lenilson.codes>
2017-12-14 13:14:19 -0300

    :construction: Started integration w/ Spotify's API

--- musicmaster/src/App.jsx
+++ musicmaster/src/App.jsx
@@ -13,6 +13,20 @@ class App extends Component {
 
   search() {
     console.log('this.state', this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    let accessToken = '';
+
+    fetch(FETCH_URL, {
+      method: 'GET',
+      headers:  {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    })
+    .then(response => response.json())
+    .then(json => console.log('json', json));
+
+    console.log('FETCH_URL', FETCH_URL);
   }
 
   render(){
