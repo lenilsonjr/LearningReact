e5822151c8484430c587b9b99766d6947571dd10
Lenilson Júnior <me@lenilson.codes>
2017-12-14 14:48:29 -0300

    :sparkles: Playabe songs!

--- musicmaster/src/Gallery.jsx
+++ musicmaster/src/Gallery.jsx
@@ -3,21 +3,68 @@ import './App.css';
 
 class Gallery extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      playingUrl: '',
+      audio: null,
+      playing: false
+    }
+  }
+
+  playAudio(previewUrl) {
+    let audio = new Audio(previewUrl);
+
+    if(!this.state.playing) {
+      audio.play();
+      this.setState({
+        playing: true,
+        playingUrl: previewUrl,
+        audio
+      })
+    } else {
+      if (this.state.playingUrl === previewUrl) {
+        this.state.audio.pause();
+        this.setState({playing: false})
+      } else {
+        this.state.audio.pause();
+        audio.play();
+        this.setState({
+          playing: true,
+          playingUrl: previewUrl,
+          audio
+        })          
+      }
+    }
+  }
+
   render() {
-    console.log('gallery', this.props);
     const { tracks } = this.props;
     return(
       <div>
         {tracks.map((track, index) => {
           const trackImg = track.album.images[0].url;
           return(
-            <div key={index} className="track">
+            <div
+              key={index}
+              className="track"
+              onClick={() => this.playAudio(track.preview_url)}
+            >
             
               <img
                 src={trackImg}
                 alt="track"
                 className="track-img"
               />
+              <div className="track-play">
+                <div className="track-play-inner">                  
+                  {
+                    this.state.playingUrl === track.preview_url && this.state.playing
+                    ? <span>| |</span>
+                    : <span>&#9654;</span>
+                  }
+                </div>
+              </div>
               <p className="track-text">{ track.name }</p>
   
             </div>  
