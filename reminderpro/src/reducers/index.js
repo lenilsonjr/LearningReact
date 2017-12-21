7d115f04bc36402f9dda4011b9e2c658f53c9438
Lenilson Júnior <me@lenilson.codes>
2017-12-21 10:19:23 -0300

    :sparkles: Storing reminders in cookies

--- reminderpro/src/reducers/index.js
+++ reminderpro/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 const reminder = (action) => {
   let { text, dueDate } = action;
@@ -17,12 +18,15 @@ const removeById = (state = [], id) => {
 const reminders = (state = [], action) => {
 
   let reminders = null;
+  state = read_cookie('reminders');
   switch(action.type) {
     case ADD_REMINDER:
       reminders = [...state, reminder(action)]
+      bake_cookie('reminders', reminders);
       return reminders;
     case DELETE_REMINDER:
       reminders = removeById(state, action.id);
+      bake_cookie('reminders', reminders);      
       return reminders;
     default:
       return state;
