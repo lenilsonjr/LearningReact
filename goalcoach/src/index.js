f458970bc1b5b4932d20e02468e62c924b6471cb
Lenilson Júnior <me@lenilson.codes>
2017-12-28 17:18:02 -0300

    :sparkles: Routes

--- goalcoach/src/index.js
+++ goalcoach/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 ReactDOM.render(
-  <div>GoalCoach</div>, document.getElementById('root')
+  <Router path="/" history={browserHistory}>
+    <Route path="/app" component={App} />
+    <Route path="/signup" component={SignUp} />    
+    <Route path="/signin" component={SignIn} />
+  </Router>, document.getElementById('root')
 )
