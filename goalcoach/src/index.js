8a11eb666422f1e323278e771d7664cc60718954
Lenilson Júnior <me@lenilson.codes>
2017-12-28 15:13:06 -0300

    :construction: Setting up new components

--- goalcoach/src/index.js
+++ goalcoach/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 ReactDOM.render(
   <div>GoalCoach</div>, document.getElementById('root')
